3729d45664bba68abf785a2ba66edfe92e605add
Jagoda Bieniek-Maggiore <jagoda.bieniek-maggiore@truckerpath.com>
2026-01-27 20:04:48 +0100

    chore(): ts config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,23 @@
-const { createDefaultPreset } = require("ts-jest");
+import { createDefaultPreset } from "ts-jest";
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
-module.exports = {
+export default {
+  preset: "ts-jest/presets/default-esm",
+  extensionsToTreatAsEsm: [".ts"],
   testEnvironment: "node",
+  testTimeout: 10000,
   transform: {
-    ...tsJestTransformCfg,
+    "^.+\\.ts$": ["ts-jest", { 
+      useESM: true,
+      tsconfig: {
+        module: "NodeNext",
+        moduleResolution: "NodeNext",
+      },
+    }],
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
   },
 };
